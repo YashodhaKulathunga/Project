66a2012ea9c4c1ef655040a0257dee23ddd02860
Mahthy Hasan <96861382+HasanMJMM@users.noreply.github.com>
2023-10-24 21:40:51 +0530

    update

--- CommanUser/sign_in.php
+++ CommanUser/sign_in.php
@@ -5,70 +5,67 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-    <link rel="stylesheet" href="stylesigninup.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="./css/signnup.css">
     <title>signin-signup</title>
 </head>
 
 <body>
 
-<?php
-if(isset($_GET["error"])){
-    
-    if($_GET["error"] == "stmtfailed"){
-         echo '<script type="text/javascript">
+    <?php
+    if (isset($_GET["error"])) {
+
+        if ($_GET["error"] == "stmtfailed") {
+            echo '<script type="text/javascript">
               alert("stmtfailed");
               window.location="sign_in.php";
           </script>';
-    }
+        }
 
-    if($_GET["error"] == "wronglogin"){
-        echo '<script type="text/javascript">
+        if ($_GET["error"] == "wronglogin") {
+            echo '<script type="text/javascript">
              alert("wrong login");
              window.location="sign_in.php";
          </script>';
-   }
-   if($_GET["error"] == "emptyinput"){
-    echo '<script type="text/javascript">
+        }
+        if ($_GET["error"] == "emptyinput") {
+            echo '<script type="text/javascript">
          alert("Please fill all fields");
          window.location="sign_in.php";
      </script>';
-}
-if($_GET["error"] == "invaliduid"){
-    echo '<script type="text/javascript">
+        }
+        if ($_GET["error"] == "invaliduid") {
+            echo '<script type="text/javascript">
          alert("Invalid Username");
          window.location="sign_in.php";
      </script>';
-}
-if($_GET["error"] == "invalidEmail"){
-    echo '<script type="text/javascript">
+        }
+        if ($_GET["error"] == "invalidEmail") {
+            echo '<script type="text/javascript">
          alert("Invalid Email");
          window.location="sign_in.php";
      </script>';
-}
-if($_GET["error"] == "passworddoesntmatch"){
-    echo '<script type="text/javascript">
+        }
+        if ($_GET["error"] == "passworddoesntmatch") {
+            echo '<script type="text/javascript">
          alert("Password is not match");
          window.location="sign_in.php";
      </script>';
-}
-if($_GET["error"] == "usernameisalreadyexist"){
-    echo '<script type="text/javascript">
+        }
+        if ($_GET["error"] == "usernameisalreadyexist") {
+            echo '<script type="text/javascript">
          alert("Username is already exist");
          window.location="sign_in.php";
      </script>';
-}
-if($_GET["error"] == "none"){
-    echo '<script type="text/javascript">
+        }
+        if ($_GET["error"] == "none") {
+            echo '<script type="text/javascript">
          alert("Successfully Registered");
          window.location="sign_in.php";
      </script>';
-}
-
-}
+        }
+    }
     ?>
 
 
@@ -84,33 +81,23 @@ if($_GET["error"] == "none"){
                     <i class="fas fa-lock"></i>
                     <input type="password" name="pwd" placeholder="Password">
                 </div>
-                <input type="submit"  name= "submit" value="Login" class="btn">
-                <p class="social-text">Or Sign in with google</p>
-                <div class="social-media">
-
-
-                    <a href="" class="social-icon">
-                        <i class="fab fa-google"></i>
-                    </a>
-
-                </div>
+                <input type="submit" name="submit" value="Login" class="btn">
                 <p class="account-text">Don't have an account? <a href="#" id="sign-up-btn2">Sign up</a></p>
             </form>
             <form action="includes/sign_in.inc.php" method="POST" class="sign-up-form">
                 <h2 class="title">Sign up</h2>
-                
-                    
-                        
-                            <div class="input-field">
-                                <i class="fas fa-user"></i>
-                                <input type="text" name="name"
-                                    placeholder="First name">
-                            </div>
-                       
-                        
-                            
-                                           
-                
+
+
+
+                <div class="input-field">
+                    <i class="fas fa-user"></i>
+                    <input type="text" name="name" placeholder="First name">
+                </div>
+
+
+
+
+
                 <div class="input-field">
                     <i class="fas fa-user"></i>
                     <input type="text" name="uid" placeholder="Username" required>
@@ -138,15 +125,7 @@ if($_GET["error"] == "none"){
                         </div>
                     </div>
                 </div>
-                <input type="submit" name= "submit" value="Sign up" class="btn">
-                <p class="social-text">Or Sign in with Google</p>
-                <div class="social-media">
-
-                    <a href="" class="social-icon">
-                        <i class="fab fa-google"></i>
-                    </a>
-
-                </div>
+                <input type="submit" name="submit" value="Sign up" class="btn">
                 <p class="account-text">Already have an account? <a href="#" id="sign-in-btn2">Sign in</a></p>
             </form>
         </div>
@@ -169,7 +148,7 @@ if($_GET["error"] == "none"){
             </div>
         </div>
     </div>
-    <script src="app.js"></script>
+    <script src="./js/signinup.js"></script>
 </body>
 
 </html>
