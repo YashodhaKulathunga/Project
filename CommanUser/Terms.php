9a323e0633e99158600840a4e2e0cabad6fc0908
Niwandi <niwandihewagama@gmail.com>
2023-11-19 19:58:14 +0530

    Privacy Policy

---
+++ CommanUser/Terms.php
@@ -0,0 +1,258 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terms and Conditions</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Ease Travels</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./css/index.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="./js/index.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <style>
+        .terms-container {
+  max-width: 900px;
+  margin: 0 auto;
+  padding: 20px;
+  font-family: Arial, sans-serif;
+  line-height: 1.5;
+}
+
+.term-section {
+  margin-bottom: 30px;
+}
+
+h2 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+h3.term-subtitle {
+  font-size: 18px;
+  margin-top: 15px;
+}
+
+p {
+  font-size: 16px;
+  margin-bottom: 10px;
+}
+    </style>
+</head>
+<body>
+    <!--Nav bar start-->
+   
+        <nav class="navbar navbar-expand-lg NAVBAR">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="./Images/Logo.png" alt="Logo" width="100" height="69" class="d-inline-block align-text-top" /></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarScroll">
+                    <ul class="navbar-nav me-auto d-flex align-items-center" style="--bs-scroll-height: 100px">
+                        <li class="nav-item align-items-center">
+                            <div class="d-flex align-items-center">
+                                <p class="SubPageTitle FIRST-NAVLINK">
+                                    Terms and Conditions
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+                    <div class="d-flex">
+                        <a href="./commanUser.php"><ion-icon name="arrow-back-circle-outline" class="mt-3 NAVLINKSICON"><span>go back</span>></ion-icon></a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+   
+    <!---Nav bar End-->
+
+    <div class="terms-container">
+        <div class="term-section">
+          <h2><b> Acceptance of Terms</b></h2> 
+          <p>
+            By accessing and using the JourneyEase Bus Management System ("the Platform"), you agree to abide by these Terms and Conditions. If you do not agree with any part of these terms, please refrain from using the Platform.
+          </p>  
+        </div>
+        <div class="term-section">
+          <h2><b>Use of the Platform</b></h2>
+          <h3 class="term-subtitle"> a. Eligibility: </h3>
+          <p>
+             You must be at least 18 years old or have the legal capacity to enter into agreements to use the Platform. If you are accessing the Platform on behalf of a company or other entity, you represent that you have the authority to bind such entity to these terms.
+          </p>
+          <h3 class="term-subtitle">b. Account Security:  </h3>
+          <p>
+            You are responsible for maintaining the security of your account. Any activities that occur under your account are your responsibility. Notify us immediately of any unauthorized use or security breach.
+          </p>
+        </div>
+        <div class="term-section">
+          <h2><b>Booking and Payments</b></h2>
+          <h3 class="term-subtitle">a. Booking Confirmation: </h3>
+          <p>
+             The booking of bus tickets through JourneyEase is subject to availability and confirmation. A booking is considered confirmed once you receive a booking confirmation from the Platform.
+          </p>
+          <h3 class="term-subtitle">b. Payment: </h3>
+          <p>
+            Payment for bus tickets is processed securely through our payment gateway. By making a booking, you agree to pay the specified amount for the selected services.
+          </p>
+          <h3 class="term-subtitle"> c. Refunds and Cancellations: </h3>
+          <p>
+            Refund and cancellation policies are outlined in the refund and cancellation policy section. Please review these policies before making any bookings.
+        </div>
+        <div class="term-section">
+          <h2><b>User Conduct</b></h2>   
+          <h3 class="term-subtitle">a. Prohibited Activities: </h3>
+          <p>
+            a. Prohibited Activities: Users must not engage in any unlawful or prohibited activities while using the Platform. This includes but is not limited to fraud, unauthorized access, and violation of applicable laws.
+          </p>
+          <h3 class="term-subtitle">b. Content Submission: : </h3>
+          <p>
+            Users are responsible for the content they submit on the Platform. Any content that violates these terms or applicable laws may be removed.
+          </p>
+          
+        </div>
+        <div class="term-section">
+            <h2><b>Privacy</b></h2>    
+            <p>
+                Data Collection: JourneyEase collects and processes personal information as outlined in our Privacy Policy. By using the Platform, you consent to the collection and processing of your data as described in the Privacy Policy.
+            </p>   
+        </div>
+        <div class="term-section">
+            <h2><b>Modifications to Terms</b></h2> 
+            <p>
+                JourneyEase reserves the right to update or modify these Terms and Conditions at any time. Users will be notified of any changes, and continued use of the Platform after such modifications constitutes acceptance of the revised terms.
+            </p>
+          </div>
+       
+    </div>
+        
+
+      <!--Footer Start-->
+    <footer class="border-top footerbackground">
+        <div class="row">
+            <div class="col-12 col-md ">
+                <span>
+                    <img class="mb-2" src="images/logo2.jpg" alt="" width="24" height="19">
+                </span>
+                <span>
+                    <p>Make Your Journy Easy</p>
+                </span>
+                <small class="d-block mb-3 text-body-secondary">&copy; 2017–2023</small>
+                <div class="row ">
+                    <div class="container firstCol">
+                        <div class="col">
+                            <a class="nav-link" aria-current="page" href="#">
+                                <span class="coustomIcon SMLF">
+                                    <ion-icon name="logo-facebook">
+                                </span>
+                            </a>
+                        </div>
+                        <div class="col">
+                            <a class="nav-link" aria-current="page" href="#">
+                                <span class="coustomIcon SMLI">
+                                    <ion-icon name="logo-instagram">
+                                </span>
+                            </a>
+                        </div>
+                        <div class="col">
+                            <a class="nav-link" aria-current="page" href="#">
+                                <span class="coustomIcon SMLW">
+                                    <ion-icon name="logo-whatsapp">
+                                </span>
+                            </a>
+                        </div>
+                        <div class="col ">
+                            <a class="nav-link" aria-current="page" href="#">
+                                <span class="coustomIcon SMLT">
+                                    <ion-icon name="logo-twitter">
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md">
+                <h5 style="color: white;">Links</h5>
+                <ul class="list-unstyled text-small">
+                    <li class="mb-1"><a class="nav-link" aria-current="page" href="#">
+                            <span class="coustomIcon">
+                                <ion-icon name="home-outline"></ion-icon>
+                            </span>
+                            <span class="coustomText">
+                                Home
+                            </span>
+                        </a>
+                    </li>
+                    <li class="mb-1"> <a class="nav-link" aria-current="page" href="#">
+                            <span class="coustomIcon">
+                                <ion-icon name="accessibility-outline"></ion-icon>
+                            </span>
+                            <span class="coustomText">
+                                About Us
+                            </span>
+                        </a>
+                    </li>
+                    <li class="mb-1">
+                        <a class="nav-link" aria-current="page" href="#">
+                            <span class="coustomIcon">
+                                <ion-icon name="headset-outline"></ion-icon>
+                            </span>
+                            <span class="coustomText">
+                                Contact Us
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h5>Policies</h5>
+                <ul class="list-unstyled text-small">
+                    <li class="mb-1"><a class="link text-decoration-none listtext" href="#">privacy Policy</a></li>
+                    <li class="mb-1"><a class="link text-decoration-none listtext" href="#">Terms & Conditions</a></li>
+                    <li class="mb-1"><a class="link text-decoration-none listtext" href="#">Ticket Policy</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h5>Contact us</h5>
+                <ul class="list-unstyled text-small">
+                    <li class="mb-1"><a class="link-secondary text-decoration-none listtext" href="../contactus/index.php">
+                            <span class="coustomIcon">
+                                <ion-icon name="location-outline"></ion-icon>
+                            </span>
+                            <span class="coustomText listtext2">
+                                No2, Passara Raod, Badulla.
+                            </span>
+                        </a>
+                    </li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">
+                            <span class="coustomIcon">
+                                <ion-icon name="call-outline"></ion-icon>
+                            </span>
+                            <span class="coustomText listtext2">
+                                +94123987456
+                            </span>
+                        </a>
+                    </li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">
+                            <span class="coustomIcon">
+                                <ion-icon name="at-outline"></ion-icon>
+                            </span>
+                            <span class="coustomText listtext2">
+                                EaseTravales@Bus.com
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+    <!--Footer End-->
+</body>
+
+</html>
---
+++ CommanUser/privacyPolicy.php
@@ -0,0 +1,267 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terms and Conditions</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Ease Travels</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./css/index.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="./js/index.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <style>
+        .terms-container {
+  max-width: 900px;
+  margin: 0 auto;
+  padding: 20px;
+  font-family: Arial, sans-serif;
+  line-height: 1.5;
+}
+
+.term-section {
+  margin-bottom: 30px;
+}
+
+h2 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+h3.term-subtitle {
+  font-size: 18px;
+  margin-top: 15px;
+}
+
+p {
+  font-size: 16px;
+  margin-bottom: 10px;
+}
+    </style>
+</head>
+<body>
+    <!--Nav bar start-->
+   
+        <nav class="navbar navbar-expand-lg NAVBAR">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="./Images/Logo.png" alt="Logo" width="100" height="69" class="d-inline-block align-text-top" /></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarScroll">
+                    <ul class="navbar-nav me-auto d-flex align-items-center" style="--bs-scroll-height: 100px">
+                        <li class="nav-item align-items-center">
+                            <div class="d-flex align-items-center">
+                                <p class="SubPageTitle FIRST-NAVLINK">
+                                    Privacy Policy
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+                    <div class="d-flex">
+                        <a href="./commanUser.php"><ion-icon name="arrow-back-circle-outline" class="mt-3 NAVLINKSICON"><span>go back</span>></ion-icon></a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+   
+    <!---Nav bar End-->
+
+    <div class="terms-container">
+        <div class="term-section">
+          <h2><b>Information We Collect</b></h2>
+          <h3 class="term-subtitle"> a. Personal Information:  </h3>
+          <p>
+           When you use JourneyEase, we may collect personal information such as your name, email address, phone number, and payment details.
+          </p>
+          <h3 class="term-subtitle">b. Usage Information: </h3>
+          <p>
+            We collect information about how you interact with our platform, including your IP address, device information, and browsing patterns.
+          </p>
+        </div>
+        <div class="term-section">
+          <h2><b>How We Use Your Information</b></h2>
+          <h3 class="term-subtitle"> a. Booking and Services: </h3>
+          <p>
+           We use your information to facilitate bus ticket bookings, provide services, and improve the functionality of the JourneyEase platform.
+          </p>
+          <h3 class="term-subtitle">b. Communication: </h3>
+          <p>
+            We may use your contact information to send you booking confirmations, updates, and important information related to your use of JourneyEase.
+          </p>
+          <h3 class="term-subtitle">c. Personalization:  </h3>
+          <p>
+            To enhance your experience, we may use your information to personalize content and recommendations.
+          </p>
+        </div>
+        <div class="term-section">
+          <h2><b>Information Sharing</b></h2>
+          <h3 class="term-subtitle">a. Service Providers: </h3>
+          <p>
+            We may share your information with third-party service providers to assist with the operation of the platform, including payment processing and data analytics.
+          </p>
+          <h3 class="term-subtitle">b. Legal Compliance: </h3>
+          <p>
+            We may disclose your information as required by law or when we believe such disclosure is necessary to protect our rights or comply with a judicial proceeding.
+          </p>
+        </div>
+        <div class="term-section">
+          <h2><b>Data Security</b></h2>   
+          
+          <p>
+            We implement industry-standard security measures to protect your personal information. However, no method of transmission over the internet or electronic storage is entirely secure. Therefore, we cannot guarantee absolute security.
+          </p>
+          
+        </div>
+        <div class="term-section">
+            <h2><b>Cookies and Similar Technologies</b></h2>   
+            
+            <p>
+                We use cookies and similar technologies to collect and track information about your usage of JourneyEase. You can manage your cookie preferences through your browser settings.
+            </p>
+           
+        </div>
+        <div class="term-section">
+            <h2><b>Your Choices</b></h2>
+            <h3 class="term-subtitle">a. Account Information: </h3>
+            <p>
+                 You can review and update your account information by logging into your JourneyEase account.
+            </p>
+            <h3 class="term-subtitle">b. Communication Preferences: </h3>
+            <p>
+                 You can manage your communication preferences by adjusting your account settings or unsubscribing from emails.
+            </p>
+          </div>
+       
+    </div>
+        
+
+      <!--Footer Start-->
+    <footer class="border-top footerbackground">
+        <div class="row">
+            <div class="col-12 col-md ">
+                <span>
+                    <img class="mb-2" src="images/logo2.jpg" alt="" width="24" height="19">
+                </span>
+                <span>
+                    <p>Make Your Journy Easy</p>
+                </span>
+                <small class="d-block mb-3 text-body-secondary">&copy; 2017–2023</small>
+                <div class="row ">
+                    <div class="container firstCol">
+                        <div class="col">
+                            <a class="nav-link" aria-current="page" href="#">
+                                <span class="coustomIcon SMLF">
+                                    <ion-icon name="logo-facebook">
+                                </span>
+                            </a>
+                        </div>
+                        <div class="col">
+                            <a class="nav-link" aria-current="page" href="#">
+                                <span class="coustomIcon SMLI">
+                                    <ion-icon name="logo-instagram">
+                                </span>
+                            </a>
+                        </div>
+                        <div class="col">
+                            <a class="nav-link" aria-current="page" href="#">
+                                <span class="coustomIcon SMLW">
+                                    <ion-icon name="logo-whatsapp">
+                                </span>
+                            </a>
+                        </div>
+                        <div class="col ">
+                            <a class="nav-link" aria-current="page" href="#">
+                                <span class="coustomIcon SMLT">
+                                    <ion-icon name="logo-twitter">
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md">
+                <h5 style="color: white;">Links</h5>
+                <ul class="list-unstyled text-small">
+                    <li class="mb-1"><a class="nav-link" aria-current="page" href="#">
+                            <span class="coustomIcon">
+                                <ion-icon name="home-outline"></ion-icon>
+                            </span>
+                            <span class="coustomText">
+                                Home
+                            </span>
+                        </a>
+                    </li>
+                    <li class="mb-1"> <a class="nav-link" aria-current="page" href="#">
+                            <span class="coustomIcon">
+                                <ion-icon name="accessibility-outline"></ion-icon>
+                            </span>
+                            <span class="coustomText">
+                                About Us
+                            </span>
+                        </a>
+                    </li>
+                    <li class="mb-1">
+                        <a class="nav-link" aria-current="page" href="#">
+                            <span class="coustomIcon">
+                                <ion-icon name="headset-outline"></ion-icon>
+                            </span>
+                            <span class="coustomText">
+                                Contact Us
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h5>Policies</h5>
+                <ul class="list-unstyled text-small">
+                    <li class="mb-1"><a class="link text-decoration-none listtext" href="#">privacy Policy</a></li>
+                    <li class="mb-1"><a class="link text-decoration-none listtext" href="#">Terms & Conditions</a></li>
+                    <li class="mb-1"><a class="link text-decoration-none listtext" href="#">Ticket Policy</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h5>Contact us</h5>
+                <ul class="list-unstyled text-small">
+                    <li class="mb-1"><a class="link-secondary text-decoration-none listtext" href="../contactus/index.php">
+                            <span class="coustomIcon">
+                                <ion-icon name="location-outline"></ion-icon>
+                            </span>
+                            <span class="coustomText listtext2">
+                                No2, Passara Raod, Badulla.
+                            </span>
+                        </a>
+                    </li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">
+                            <span class="coustomIcon">
+                                <ion-icon name="call-outline"></ion-icon>
+                            </span>
+                            <span class="coustomText listtext2">
+                                +94123987456
+                            </span>
+                        </a>
+                    </li>
+                    <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">
+                            <span class="coustomIcon">
+                                <ion-icon name="at-outline"></ion-icon>
+                            </span>
+                            <span class="coustomText listtext2">
+                                EaseTravales@Bus.com
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+    <!--Footer End-->
+</body>
+
+</html>
