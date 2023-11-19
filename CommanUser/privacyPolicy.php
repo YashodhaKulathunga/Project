445cb1b17112f63a040461aaaa287e407bf75123
Chamara D Rodrigo <108047725+ChamaraRodrigo@users.noreply.github.com>
2023-11-19 20:25:33 +0530

    update

--- CommanUser/privacyPolicy.php
+++ CommanUser/privacyPolicy.php
@@ -141,15 +141,16 @@ p {
     </div>
         
 
-      <!--Footer Start-->
+     <!--Footer Start-->
     <footer class="border-top footerbackground">
         <div class="row">
             <div class="col-12 col-md ">
                 <span>
-                    <img class="mb-2" src="images/logo2.jpg" alt="" width="24" height="19">
+                    <img class="mb-2" src="images/logo2.jpg" alt="" width="125" height="87">
                 </span>
                 <span>
-                    <p>Make Your Journy Easy</p>
+                    <p style="color: pink;">Make Your Journey Easy</p>
+
                 </span>
                 <small class="d-block mb-3 text-body-secondary">&copy; 2017–2023</small>
                 <div class="row ">
@@ -186,9 +187,9 @@ p {
                 </div>
             </div>
             <div class="col-6 col-md">
-                <h5 style="color: white;">Links</h5>
-                <ul class="list-unstyled text-small">
-                    <li class="mb-1"><a class="nav-link" aria-current="page" href="#">
+                <h5 style="color: pink;"">Links</h5>
+                <ul class=" list-unstyled text-small">
+                    <li class="mb-1"><a class="nav-link" aria-current="page" href="commanUser.php">
                             <span class="coustomIcon">
                                 <ion-icon name="home-outline"></ion-icon>
                             </span>
@@ -197,17 +198,18 @@ p {
                             </span>
                         </a>
                     </li>
-                    <li class="mb-1"> <a class="nav-link" aria-current="page" href="#">
+                    <li class="mb-1"> <a class="nav-link" aria-current="page" href="aboutus.php">
                             <span class="coustomIcon">
                                 <ion-icon name="accessibility-outline"></ion-icon>
                             </span>
-                            <span class="coustomText">
-                                About Us
-                            </span>
-                        </a>
+
+                            <span class="coustomText">About Us </span></a>
+
+
+
                     </li>
                     <li class="mb-1">
-                        <a class="nav-link" aria-current="page" href="#">
+                        <a class="nav-link" aria-current="page" href="Contactus.php">
                             <span class="coustomIcon">
                                 <ion-icon name="headset-outline"></ion-icon>
                             </span>
@@ -216,18 +218,18 @@ p {
                             </span>
                         </a>
                     </li>
-                </ul>
+                    </ul>
             </div>
             <div class="col-6 col-md">
-                <h5>Policies</h5>
+                <h5 style="color: pink;">Policies</h5>
                 <ul class="list-unstyled text-small">
-                    <li class="mb-1"><a class="link text-decoration-none listtext" href="#">privacy Policy</a></li>
-                    <li class="mb-1"><a class="link text-decoration-none listtext" href="#">Terms & Conditions</a></li>
-                    <li class="mb-1"><a class="link text-decoration-none listtext" href="#">Ticket Policy</a></li>
+                    <li class="mb-1"><a class="link text-decoration-none listtext" href="privacyPolicy.php">privacy Policy</a></li>
+                    <li class="mb-1"><a class="link text-decoration-none listtext" href="Terms.php">Terms & Conditions</a></li>
+                    <li class="mb-1"><a class="link text-decoration-none listtext" href="TicketPolicy.php">Ticket Policy</a></li>
                 </ul>
             </div>
             <div class="col-6 col-md">
-                <h5>Contact us</h5>
+                <h5 style="color: pink;">Contact us</h5>
                 <ul class="list-unstyled text-small">
                     <li class="mb-1"><a class="link-secondary text-decoration-none listtext" href="../contactus/index.php">
                             <span class="coustomIcon">
@@ -260,6 +262,11 @@ p {
             </div>
         </div>
     </footer>
+    <script>
+        $(document).ready(function() {
+            $('#myModal').modal('show');
+        });
+    </script>
 
     <!--Footer End-->
 </body>
