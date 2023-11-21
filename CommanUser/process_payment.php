e711d69325354445f97cd76eeb7f6ba411604fb2
Mahthy Hasan <96861382+HasanMJMM@users.noreply.github.com>
2023-11-22 02:09:23 +0530

    update

--- CommanUser/process_payment.php
+++ CommanUser/process_payment.php
@@ -1,9 +1,13 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 session_start();
 $userID =  $_SESSION["userid"];
 
 echo $userID;
-
 
 $serverName = "localhost";
 $username = "root";
@@ -14,8 +18,9 @@ $conn = new mysqli($serverName, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-// getting row count fot ticket id
-$query = "SELECT COUNT(*) as count FROM ticket_reservation WHERE Status = 'Unpaid' AND 	UserID = '$userID' ";
+
+// getting row count for ticket id
+$query = "SELECT COUNT(*) as count FROM ticket_reservation WHERE Status = 'Unpaid' AND UserID = '$userID' ";
 $result = mysqli_query($conn, $query);
 if (!$result) {
     die("Query failed: " . mysqli_error($connection));
@@ -25,7 +30,7 @@ $row = mysqli_fetch_assoc($result);
 $count = $row['count'];
 $amount = 2000 * $count;
 // getting Ticket_ID from Database
-$sql2 = "SELECT 	Ticket_ID FROM ticket_reservation WHERE status = 'Unpaid' AND UserID = '$userID' LIMIT 1";
+$sql2 = "SELECT Ticket_ID FROM ticket_reservation WHERE status = 'Unpaid' AND UserID = '$userID' LIMIT 1";
 $result2 = $conn->query($sql2);
 if ($result2) {
     if ($result2->num_rows > 0) {
@@ -36,6 +41,7 @@ if ($result2) {
 } else {
     echo "Error executing the query: " . $conn->error;
 }
+
 $otp = mt_rand(100000, 999999);
 $email = $_POST['emailadd'];
 $cardName = $_POST['cardName'];
@@ -45,6 +51,7 @@ $expYear = $_POST['expYear'];
 $cvv = $_POST['cvv'];
 $status = 'UnPaid';
 $paymentID = '';
+
 $sql = "SELECT MAX(Payment_ID) AS max_payment_id FROM income_payment";
 $result = $conn->query($sql);
 if ($result && $row = $result->fetch_assoc()) {
@@ -60,6 +67,7 @@ if ($result && $row = $result->fetch_assoc()) {
         $paymentID = 'PAY0001';
     }
 }
+
 // sent otp to customer
 
 // Insert the data into the database
@@ -70,8 +78,42 @@ if ($stmt) {
     $stmt->bind_param("ssssssssssss", $paymentID, $ticketID, $userID, $email, $cardName, $cardNumber, $expMonth, $expYear, $cvv, $amount, $otp, $status);
 
     if ($stmt->execute()) {
-        $_SESSION['payID'] = $paymentID;
-        header("Location: varifyOTP.php");
+        // Send email
+        require './mail/Exception.php';
+        require './mail/PHPMailer.php';
+        require './mail/SMTP.php';
+
+        $mail = new PHPMailer(true);
+
+        // Server settings
+        $mail->SMTPDebug = 0; // Enable verbose debug output
+        $mail->isSMTP(); // Send using SMTP
+        $mail->Host = 'smtp.gmail.com'; // Set the SMTP server to send through
+        $mail->SMTPAuth = true; // Enable SMTP authentication
+        $mail->Username = 'maheng907@gmail.com'; // SMTP username
+        $mail->Password = 'saxf fnjw bzhq izej'; // SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Enable implicit TLS encryption
+        $mail->Port = 465; // TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+        // Recipients
+        $mail->setFrom('maheng907@gmail.com');
+        $mail->addAddress($email); // Add a recipient
+        // Name is optional
+
+        // Content
+        $mail->isHTML(true); // Set email format to HTML
+        $mail->Subject = 'Journey Ease Verification OTP';
+        $message = "Dear " . $cardName . "You Booked seats through our system <br><br> That is Total cost of " .  $amount . "Rupees will deduct from your debit card <br><br> This Your OTP pin  " . $otp . " You can complete your payment using this OTP number<br><br> Don't share this with anyone";
+        $mail->Body = $message;
+
+        try {
+            $mail->send();
+            $_SESSION['payID'] = $paymentID;
+            header("Location: varifyOTP.php");
+            exit(); // Ensuring script stops here
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
     } else {
         echo "Error: " . $stmt->error;
     }
